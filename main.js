35c22023549938cf2a80cb225dc0d80b325132db
emilykpritchard <mailtoemilykpritchard@me.com>
2022-11-03 16:35:30 +0000

    KATA READY

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 /* 
 THE ENIGMA MACHINE!!!!!
 Arshi has uploaded his learnings for the day but unfortunately the code-kremlin have gotten to them first, In this Kata you must decode the given string in order to discover your daily learnings!
+Your input will always be a string and will only contain lowercase letters. 
 Your function MUST :
 Only return characters.
-must all be lowercase.
-must perfectly match encrypted learnings.
+Must all be lowercase.
+Must perfectly match encrypted learnings.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
   
 
-
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 export function Enigma (code)  {
-    const code = "Java$Scrip2t was i14nve56nt12ed by Bre122nd764an Eic1231h in n13i4ne1te4en nin67et165y fiv35e, an00d b123ec4536ame an E141C6MA sta67867867ndar123d in nine24234teen sev4525enty s2ev7en"
-
-    return "JavaScript was invented by Brendan Eich in nineteen ninety five, and became an ECMA standard in nineteen ninety seven"
-  }
+  
+  };
 
   
