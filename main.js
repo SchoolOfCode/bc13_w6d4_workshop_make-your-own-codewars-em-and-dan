9095804c51b2b0773578889b8365186a3494497c
Cmndgrab <110107052+Cmndgrab@users.noreply.github.com>
2022-11-03 14:32:02 +0000

    skeleton up

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 /* 
-👉 Write your kata here!
+THE ENIGMA MACHINE!!!!!
+Arshi has uploaded his learnings for the day but unfortunately the code-kremlin have gotten to them first, In this Kata you must decode the given string in order to discover your daily learnings!
+Your function MUST :
+Only return characters.
+must all be lowercase.
+must perfectly match encrypted learnings.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+  
+
+
+
+export function Enigma (code)  {
+    const code = "Java$Scrip2t was i14nve56nt12ed by Bre122nd764an Eic1231h in n13i4ne1te4en nin67et165y fiv35e, an00d b123ec4536ame an E141C6MA sta67867867ndar123d in nine24234teen sev4525enty s2ev7en"
+
+    return "JavaScript was invented by Brendan Eich in nineteen ninety five, and became an ECMA standard in nineteen ninety seven"
+  }
+
+  
--- main.test.js
+++ main.test.js
@@ -1,1 +1,14 @@
 //👉 Write your tests below here:
+import {test,expect} from '@jest/globals'
+import {Enigma} from "./main"
+
+const myCode = "Java$Scrip2t was i14nve56nt12ed by Bre122nd764an Eic1231h in n13i4ne1te4en nin67et165y fiv35e, an00d b123ec4536ame an E141C6MA sta67867867ndar123d in nine24234teen sev4525enty s2ev7en"
+
+
+test(`Should decode Enigma machine`, () => {
+ expect(Enigma("l2o1z w6e5n4t t2o su1p5e4r1cuts")).toBe("loz went to supercuts")
+})
+
+test(`should not contain numbers`, () => {
+    expect(Enigma(code)).not.toContain(Number)
+})
