98e18c8f500e22f7eb61acd51d499d546b620b74
emilykpritchard <mailtoemilykpritchard@me.com>
2022-11-03 16:38:12 +0000

    Version 2

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ Must perfectly match encrypted learnings.
 //👉 Write the function your CodeWarriors will start with below here:
   
 
-const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
 export function Enigma (code)  {
   
   };
